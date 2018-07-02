3acc43d76555b283e3482e3b2cb119a0f6b17683
suzuki.r <getty2017@gmail.com>
2018-07-02 22:33:49 +0900

    set link to sign up

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+end
